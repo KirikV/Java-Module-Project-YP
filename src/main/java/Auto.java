58fdc744dc553887bc71cd561ee21b8c11eca215
KirikV <anoprienkok@gmail.com>
2025-01-12 23:36:46 +0300

    Рабочий код

--- src/main/java/Auto.java
+++ src/main/java/Auto.java
@@ -1,7 +1,20 @@
-public class Auto {
-    String name;
+import java.util.Objects;
 
-    public Auto(String autoName) {
+public class Auto {
+
+    String name;
+    int speed;
+
+    public Auto(String autoName, int sPeed) {
+        speed = sPeed;
         name = autoName;
+    }
+
+    @Override
+    public String toString() {
+        return "Auto{" +
+                "name='" + name + '\'' +
+                ", speed=" + speed +
+                '}';
     }
 }
--- src/main/java/RaceLaMan.java
+++ src/main/java/RaceLaMan.java
@@ -3,21 +3,51 @@ import java.util.Scanner;
 
 class RaceLaMan {
     ArrayList<Auto> laMan = new ArrayList<>();
+    Scanner console = new Scanner(System.in);
 
     public void start() {
-        executeCommand();
-    }
+        System.out.println("Мы рады приветствовать на гонке Ле ман 24!");
+        System.out.println("Введите 'Привет'");
+        while (true) {
+            String command = console.nextLine();
+            if (command.equals("Завершить")) {
+                System.out.println("Программа завершена! Спасибо, что зашли к нам на гонку!");
+                break;
+            } else {
+                executeCommand(command);
+            }
 
-    public void executeCommand() {
-        Scanner console = new Scanner(System.in);
-        String command = console.nextLine();
-        if (command.equals("Привет")) {
-            System.out.println("Время назвать модели авто наших участников!");
-            executePrint();
         }
     }
 
-    public void executePrint() {
-        laMan.add(new Auto("Цивик"));
+    private void executeCommand(String command) {
+        if (command.equals("Привет")) {
+            System.out.println("Время назвать модели авто наших участников!");
+            System.out.println("Введите авто участника и его скорость");
+            executeCreate();
+        }
+    }
+
+    private void executeCreate() {
+        while (laMan.size() < 3) {
+            String name = console.nextLine();
+            int speed = console.nextInt();
+            console.nextLine();
+            laMan.add(new Auto(name, speed));
+            System.out.println("Давайте поприветствуем нашего участника на " + name + ", " +
+                    "его скорость равна " + speed + " км/ч");
+        }
+        System.out.println("Отлично, для начала гонки введите 'СТАРТ'");
+        compareSpeed();
+    }
+
+    private void compareSpeed() {
+        String go = console.nextLine();
+        if (go.equals("СТАРТ")) {
+            System.out.println("ГОНКА НАЧАЛАСЬ \nУПОРНАЯ БОРЬБА");
+        } else if (speed(0) > speed(1),speed(2)){
+            System.out.println();
+
+        }
     }
 }
